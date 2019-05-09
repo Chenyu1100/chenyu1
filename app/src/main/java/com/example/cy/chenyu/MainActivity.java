8f787bcc6c00a90983eff4310bc096e024df57f7
Chenyu1100 <www.chenyu.10100@foxmail.com>
2019-05-09 21:14:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cy/chenyu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cy.chenyu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
